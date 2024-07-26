f1266106305c4de7f026b43ac67f01f9a99c7354
csalitre <salitrecristian@gmail.com>
2024-07-25 21:02:45 -0400

    API notes

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # dplock-device
 Embedded code development for the Senior Design project DPLOCK device.
+
+## Development Notes
+
+1. **Key API References:**
+   - **Event Loop Library** -> How to handle events
+   - **Sleep Modes** -> Enable and disable low power mode
+   - **GPIO & RTC GPIO** -> Set directions & pull modes of registers
+   - **Bluetooth Low Energy** -> How to handle GAP, GATT Define, GATT Server, GATT Client, BluFi
+     - **Client** -> Allows connection and data reading from other BLE devices (e.g., smartphones)
+     - **Event Handling** -> Handling data receptions, incoming connections, and other BLE activities
+   - **Error Handling** -> Check for situations such as invalid states
+   - **Watchdogs** -> For reliability and fault tolerance
+   - **Motor Control PWM (MCPWM)** -> For DC servo motor
+
--- main/dplock-device.c
+++ main/dplock-device.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
 
-void app_main(void)
-{
+#define TAG "rgb_led_example"
+
+void flash_rgb(void *pvParameters) {
+  const int ledPin = 48; // IO48 indicated on MCU
+
+  ESP_LOGI(TAG, "Flashing RGB LED");
+
+  gpio_set_direction(ledPin, GPIO_MODE_OUTPU);
+
+  // Set RGB Color directly
+  gpio_set_level(ledPin, 255); // White
+
+  vTaskDelay(pdMS_TO_TICKS(1000));
+
+  gpio_set_level(ledPin, 255 << 8 | 255 << 16); // Red (Green and Blue are shifted into thier respective places)
+
+  vTaskDelay(pdMS_TO_TICKS(1000));
+
+  gpio_set_level(ledPin, 255 << 8); // Green
+
+  vTaskDelay(pdMS_TO_TICKS(1000));
+
+  gpio_set_level(ledPin, 255 << 16); // Blue
+
+  vTaskDelay(pdMS_TO_TICKS(1000));
+
+  gpio_set_direction(ledPin, GPIO_MODE_INPUT);
+
+
+
+void app_main(void) {
+  ESP_LOGI(TAG, "Starting rgb_led_example");
+
+  xTaskCreate(&flash_rgb, "rgb_flash", 2048, NULL, 5, NULL);
 
 }
